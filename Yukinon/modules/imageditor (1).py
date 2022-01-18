ab0e3176b8ac1852f76f6ce65a4a7ccf8e471650
Madhusudhan127265588 <96523976+Madhusudhan127265588@users.noreply.github.com>
2022-01-18 22:49:32 +0530

    Update imageditor (1).py

--- Yukinon/modules/imageditor (1).py
+++ Yukinon/modules/imageditor (1).py
@@ -53,7 +53,7 @@ from TGNRobot.Addons.ImageEditor.edit_5 import (  # pylint:disable=import-error
     scanlineglitch_4,
     scanlineglitch_5,
 )
-from TGNRobot import pbot as Client
+from Yukinon import pbot as Client
 
 lel = 00000000
 # pylint:disable=import-error
